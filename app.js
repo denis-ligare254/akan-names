0ff57fa0a672c705c12d875dde7a198244fa34c5
denis <mrisholigare@gmail.com>
2020-10-02 09:45:45 +0300

    i am adding a readme.md

--- app.js
+++ app.js
@@ -2,4 +2,4 @@
 let days = ['monday','tuesday','wednesday','thursday','friday','sartuday','sunday'];
 let mnames =['kwadwo','kwabena','kwaku','yaw','kofi','kwame','kwasi'];
 let fnames =['adwoa','abenaa','akua','yaa','afua','ama','akosua'];
-// end of first array
+// 
