76239a63abf261a651775c08b19c0f6368589c44
denis <mrisholigare@gmail.com>
2020-10-13 20:30:44 +0300

    final commit

--- app.js
+++ app.js
@@ -1,5 +1,43 @@
-// array  declaration
-let days = ['monday','tuesday','wednesday','thursday','friday','sartuday','sunday'];
-let mnames =['kwadwo','kwabena','kwaku','yaw','kofi','kwame','kwasi'];
-let fnames =['adwoa','abenaa','akua','yaa','afua','ama','akosua'];
-// 
+//using arrays to store names
+
+var male = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+
+var female = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+
+var dateWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+
+function Clickme() {
+ var day = parseInt(document.getElementById("day").value);
+
+ var month = parseInt(document.getElementById("month").value);
+
+ var year = parseInt(document.getElementById("year").value);
+
+ var gender = radioInfo();
+
+ var dayWeek = new Date(year + "/" + month + "/" + day);
+ var d = dayWeek.getDay();
+ var name = "";
+ if (gender === "male") {
+  name = male[d];
+ }
+ else {
+  name = female[d];
+ }
+
+ alert("You were born on " + dateWeek[d] + " and your Akan name is " + name);
+ document.getElementById('akan').innerHTML = "You were born on " + daysOfTheWeek[d] + " and your Akan name is: " + maleNames[d];
+}
+function radioInfo() {
+ var radio = document.getElementsByName('gender');
+ for (var i = 0; i < radio.length; i++) {
+  if (radio[i].checked == true) {
+
+
+   gender = radio[i].value;
+
+   return gender;
+  }
+ }
+}
